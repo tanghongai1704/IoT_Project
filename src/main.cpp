d21ea1adab86e751d76142435f54ee62ae3d74d4
giantkduy84210 <duy.pham84210@hcmut.edu.vn>
2026-04-27 15:36:37 +0700

    Refactor code structure for improved readability and maintainability

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,15 @@
 #include "led_blinky.h"
 #include "neo_blinky.h"
 #include "temp_humi_monitor.h"
-// #include "mainserver.h"
+#include "mainserver.h"
 #include "tinyml.h"
 #include "coreiot.h"
 
 // include task
 #include "task_check_info.h"
 #include "task_toogle_boot.h"
-#include "task_wifi.h"
-#include "task_webserver.h"
+// #include "task_wifi.h"
+// #include "task_webserver.h"
 // #include "task_core_iot.h"
 
 void setup()
@@ -21,11 +21,10 @@ void setup()
   xTaskCreate(led_blinky, "Task LED Blink", 2048, NULL, 2, NULL);
   xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
   xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
-  xTaskCreate(webserver_task, "WebServer Task", 8192, NULL, 2, NULL);
+  xTaskCreate(main_server_task, "WebServer Task", 8192, NULL, 2, NULL);
   // xTaskCreate(tiny_ml_task, "Tiny ML Task", 2048, NULL, 2, NULL);
   // xTaskCreate(coreiot_task, "CoreIOT Task", 4096, NULL, 2, NULL);
   xTaskCreate(Task_Toogle_BOOT, "Task_Toogle_BOOT", 4096, NULL, 2, NULL);
-  xTaskCreate(wifi_task, "WiFi Task", 4096, NULL, 3, NULL);
 }
 
 void loop()
