a63c9db43951cf4fb771d83b3ed463c71f550f0c
Tang Ai <hongai17041997@gmail.com>
2026-04-27 11:29:06 +0700

    Change CORE_IOT_PORT type from int to String and comment out unused task creation calls in setup function

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,12 @@ void setup()
   Serial.begin(115200);
   check_info_File(0);
 
-  xTaskCreate(led_blinky, "Task LED Blink", 2048, NULL, 2, NULL);
-  xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
+  // xTaskCreate(led_blinky, "Task LED Blink", 2048, NULL, 2, NULL);
+  // xTaskCreate(neo_blinky, "Task NEO Blink", 2048, NULL, 2, NULL);
   xTaskCreate(temp_humi_monitor, "Task TEMP HUMI Monitor", 2048, NULL, 2, NULL);
   xTaskCreate(webserver_task, "WebServer Task", 8192, NULL, 2, NULL);
-  xTaskCreate(tiny_ml_task, "Tiny ML Task", 2048, NULL, 2, NULL);
-  // xTaskCreate(coreiot_task, "CoreIOT Task", 4096, NULL, 2, NULL);
+  // xTaskCreate(tiny_ml_task, "Tiny ML Task", 2048, NULL, 2, NULL);
+  xTaskCreate(coreiot_task, "CoreIOT Task", 4096, NULL, 2, NULL);
   // xTaskCreate(Task_Toogle_BOOT, "Task_Toogle_BOOT", 4096, NULL, 2, NULL);
 }
 
